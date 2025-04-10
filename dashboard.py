d04104c3494d2db3f4b4f5fb94c7f3ee0778c30f
parasnath-tech <parasnath4118@gmail.com>
2025-04-10 15:17:13 +0530

    Update dashboard.py

--- dashboard.py
+++ dashboard.py
@@ -5,7 +5,7 @@ import plotly.express as px
 # Set page config
 st.set_page_config(
     page_title="Oracle Cloud Cost Optimizer",
-    page_icon="https://www.datacenterplanet.com/wp-content/uploads/2021/02/oracle-cloud-logo.jpg",
+    page_icon="https://i.ibb.co/zVJb4Bdt/oracle-cloud-logo-removebg-preview.png",
     layout="wide"
 )
 st.image("https://logodix.com/logo/692804.png", width=400)
