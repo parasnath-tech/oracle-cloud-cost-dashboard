f0f2f1996a47a10c3fa3174893c370ae75493c8d
parasnath-tech <parasnath4118@gmail.com>
2025-04-10 15:39:12 +0530

    Update dashboard.py

--- dashboard.py
+++ dashboard.py
@@ -18,7 +18,7 @@ with col1:
     st.subheader("📊 Real-time cost insights for Oracle Cloud Infrastructure")
 
 with col2:
-    st.image("https://www.tecnovait.com/wp-content/uploads/2023/11/oracle-servicio-cloud-en.png", width=80)
+    st.image("https://i.ibb.co/KpGPBkDC/Oracle-Logo-History.png", width=80)
 
 
 # Sidebar setup
